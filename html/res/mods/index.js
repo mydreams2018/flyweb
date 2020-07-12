2b043e932b9fe3c263eca0c8f522d0e987de3236
kungreat <mydreams2018@outlook.com>
2020-07-13 07:53:29 +0800

    search

--- html/res/mods/index.js
+++ html/res/mods/index.js
@@ -493,6 +493,35 @@ layui.define(['layer', 'laytpl', 'form', 'element', 'upload', 'util', 'carousel'
     });
   }
 
+
+  //搜索
+  $('.fly-search').on('click', function(){
+    layer.open({
+      type: 1
+      ,title: false
+      ,closeBtn: false
+      //,shade: [0.1, '#fff']
+      ,shadeClose: true
+      ,maxWidth: 10000
+      ,skin: 'fly-layer-search'
+      ,content: ['<form action="http://cn.bing.com/search">'
+        ,'<input autocomplete="off" placeholder="搜索内容，回车跳转" type="text" name="q">'
+      ,'</form>'].join('')
+      ,success: function(layero){
+        var input = layero.find('input');
+        input.focus();
+
+        layero.find('form').submit(function(){
+          var val = input.val();
+          if(val.replace(/\s/g, '') === ''){
+            return false;
+          }
+          input.val('site:layui.com '+ input.val());
+      });
+      }
+    })
+  });
+
   //新消息通知
   fly.newmsg();
 
