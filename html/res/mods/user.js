b995e732f24243b9492a7553989eb417f835fe78
kungreat <mydreams2018@outlook.com>
2020-12-27 19:03:42 +0800

    我的信息页面完成

--- html/res/mods/user.js
+++ html/res/mods/user.js
@@ -111,30 +111,99 @@ layui.define(['fly', 'element'], function(exports){
         dom.minemsg.html('<div class="fly-none">您暂时没有最新消息</div>');
         }
       }
-
-    //阅读后删除
+      ,deldataMsg = 0
+      ,resDataTemp = null
+      ,queryMessage = function(){
+      //查询@信息
+         $.ajax({
+          url: "/api/userMessage/selectAll",
+          type: "post",
+          async: false,
+          success: function (res) {
+            dom.minemsg.empty();
+            resDataTemp = null;
+            deldataMsg = 0;
+           if(res && !res.toString().match('<!DOCTYPE html>') &&res.length > 0){
+              for(var x=0;x<res.length;x++) {
+                var resData = res[x];
+                var rt = messageStr.replace(/{classId}/g,resData.classId).replace(/{id}/g,resData.id)
+                  .replace(/{alias}/g,resData.srcAlias).replace(/{portId}/g,resData.portId)
+                  .replace(/{receiveDate}/g,resData.receiveDate);
+                dom.minemsg.append(rt);
+                deldataMsg++;
+              }
+             resDataTemp = res;
+            }else{
+              delEnd(true);
+           }
+          },
+          error: function () {
+          layer.msg("获得数据出错", {icon: 5});
+          delEnd(true);
+          }
+         });
+      };
+    //阅读
     dom.minemsg.on('click', '.mine-msg li .fly-delete', function(){
       var othis = $(this).parents('li'), id = othis.data('id');
-      console.log('阅读后删除');
-      // if(res.status == 1){
-      //   othis.remove();
-      //   delEnd();
-      // }
+      console.log('阅读');
+      if(id && resDataTemp && resDataTemp.length > 0){
+          for(var x=0;x<resDataTemp.length ;x++){
+             if(resDataTemp[x].id == id){
+               $.ajax({
+                 url: "/api/detailsText/selectByPrimaryKey",
+                 type: "post",
+                 async: false,
+                 data:{
+                   "id":resDataTemp[x].detailsId,
+                   "classId":resDataTemp[x].classId
+                 },
+                 success: function (res) {
+                    if(res && res.id && res.detailsText){
+                      var detaieText = fly.content(res.detailsText);
+                      layer.open({
+                        type: 1
+                        ,title: '预览'
+                        ,shade: false
+                        ,area: ['100%', '100%']
+                        ,scrollbar: false
+                        ,content: '<div class="detail-body" style="margin:20px;">'+ detaieText +'</div>'
+                      });
+                    }else{
+                      layer.msg("获得数据出错", {icon: 5});
+                    }
+                 },
+                 error: function () {
+                   layer.msg("获得数据出错", {icon: 5});
+                 }
+               });
+             }
+          }
+      }else{
+        layer.msg("获得数据出错", {icon: 5});
+      }
     });
 
     //删除全部
     $('#LAY_delallmsg').on('click', function(){
-      var othis = $(this);
-      layer.confirm('确定清空吗？', function(index){
-        layer.close(index);
-        console.log('确定清空吗');
-        // if(res.status == 1){
-        //   othis.addClass('layui-hide');
-        //   delEnd(true);
-        // }
-      });
+      if(deldataMsg > 0){
+        layer.confirm('确定清空吗？', function(index){
+          layer.close(index);
+          $.ajax({
+            url: "/api/userMessage/deleteByAccount",
+            type: "post",
+            async: false,
+            data:{
+              "deleteNum":deldataMsg
+            },
+            success: function (res) {
+              queryMessage();
+            }
+          });
+        });
+      }
     });
-
+    queryMessage();
   };
 
   dom.minemsg[0] && gather.minemsg();
